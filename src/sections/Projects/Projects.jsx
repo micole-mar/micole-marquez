63a02943f405a901c3a57ded5d8173995ae46cc8
Micole M *:･ﾟ✧*:･ﾟ✧ <71249945+micole-mar@users.noreply.github.com>
2024-07-17 08:34:26 +1200

    foundation
    built foundation of website

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,19 @@
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/viberr.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png';
+import fitLift from '../../assets/fitlift.png';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,19 @@
+import styles from './SkillsStyles.module.css';
+import checkMarkIconDark from '../../assets/checkmark-dark.svg';
+import checkMarkIconLight from '../../assets/checkmark-light.svg';
+import SkillList from '../../common/SkillList';
+import { useTheme } from '../../common/ThemeContext';
+
+function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      
+    </section>
+  );
+}
+
+export default Skills;
