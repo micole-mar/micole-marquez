01635bd39a369011534262bf22fe8aa9acf66561
Micole M *:･ﾟ✧*:･ﾟ✧ <71249945+micole-mar@users.noreply.github.com>
2024-07-18 10:10:03 +1200

    Update Footer.jsx

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,8 +4,7 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Harris johnsen. <br />
-        All rights reserved.
+        &copy; 2024  Micole Marquez. <br />
       </p>
     </section>
   );
