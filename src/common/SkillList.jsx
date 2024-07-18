5a40e9298d1699990bf8d00880ee4165937f3852
Micole M *:･ﾟ✧*:･ﾟ✧ <71249945+micole-mar@users.noreply.github.com>
2024-07-19 10:42:02 +1200

    Skills Section
    Created skills section

--- src/common/SkillList.jsx
+++ src/common/SkillList.jsx
@@ -1,9 +1,18 @@
-function SkillList({ src, skill }) {
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import styles from './SkillListStyles.module.css';
+
+function SkillList({ skills }) {
   return (
-    <span>
-      <img src={src} alt="Checkmark icon" />
-      <p>{skill}</p>
-    </span>
+    <div className={styles.skillList}>
+      {skills.map((skill, index) => (
+        <span key={index} className={styles.skillItem}>
+          <FontAwesomeIcon icon={faCheck} className={styles.icon} />
+          <p>{skill}</p>
+        </span>
+      ))}
+    </div>
   );
 }
 
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,17 +1,35 @@
+import React from 'react';
 import styles from './SkillsStyles.module.css';
-import checkMarkIconDark from '../../assets/checkmark-dark.svg';
-import checkMarkIconLight from '../../assets/checkmark-light.svg';
 import SkillList from '../../common/SkillList';
-import { useTheme } from '../../common/ThemeContext';
 
 function Skills() {
-  const { theme } = useTheme();
-  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
-
   return (
     <section id="skills" className={styles.container}>
-      <h1 className="sectionTitle">Skills</h1>
-      
+      <h1 className={styles.sectionTitle}>Skills</h1>
+      <div className={styles.skillCategories}>
+        <div className={styles.skillCategory}>
+          <h2>Technical Skills</h2>
+          <div className={styles.skillSubCategories}>
+            <div className={styles.skillSubCategory}>
+              <h3>Software Development</h3>
+              <SkillList skills={['Java', 'Python', '.NET, C#', 'HTML/CSS/JavaScript', 'React Framework']} />
+            </div>
+            <div className={styles.skillSubCategory}>
+              <h3>Data Skills</h3>
+              <SkillList skills={['SQL', 'Python Pandas', 'Microsoft Excel', 'Tableau', 'Databases']} />
+            </div>
+            <div className={styles.skillSubCategory}>
+              <h3>Other Skills</h3>
+              <SkillList skills={['AWS', 'Cloud Computing Basics', 'GitHub', 'Agile Methodologies', 'Networking Fundamentals']} />
+            </div>
+          </div>
+        </div>
+        <hr className={styles.separator} />
+        <div className={styles.skillCategory}>
+          <h2>Soft Skills</h2>
+          <SkillList skills={['Active-listening', 'Communication', 'Teamwork', 'Problem-Solving', 'Adaptability']} />
+        </div>
+      </div>
     </section>
   );
 }
