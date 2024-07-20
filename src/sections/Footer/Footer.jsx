7102215b51b290842cc07493c1f83f734bac89e7
Micole M *:･ﾟ✧*:･ﾟ✧ <71249945+micole-mar@users.noreply.github.com>
2024-07-20 21:01:53 +1200

    footer

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,11 +1,15 @@
+import React from 'react';
 import styles from './FooterStyles.module.css';
+import cloudImage from '../../assets/clouds.svg'; 
 
 function Footer() {
   return (
-    <section id="footer" className={styles.container}>
-      <p>
-        designed by Micole Marquez. <br />
-      </p>
+    <section
+      id="footer"
+      className={styles.container}
+      style={{ backgroundImage: `url(${cloudImage})` }}
+    >
+      <p>designed by Micole Marquez.<br /></p>
     </section>
   );
 }
